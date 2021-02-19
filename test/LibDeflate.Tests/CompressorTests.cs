a3237d5cda825a7ab6188f183fb584b0f5ffdc35
J. Zebedee <zebedee@code.gripe>
2021-02-19 01:44:52 -0600

    Add CompressorTests

---
+++ test/LibDeflate.Tests/CompressorTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Compression;
+using System.Linq;
+using Xunit;
+
+namespace LibDeflate.Tests
+{
+    public class CompressorTests
+    {
+        public delegate ReadOnlyMemory<byte> BclInflater(ReadOnlyMemory<byte> deflatedInput);
+
+        public static IEnumerable<object[]> Compressors(int compressionLevel)
+        {
+            var input = GetRandomBuffer(length: 0x7900);
+            yield return new object[] { new DeflateCompressor(compressionLevel), input, (BclInflater)Inflate };
+            yield return new object[] { new ZlibCompressor(compressionLevel), input, (BclInflater)ZlibInflate };
+            yield return new object[] { new GzipCompressor(compressionLevel), input, (BclInflater)GzipInflate };
+
+            static ReadOnlyMemory<byte> Inflate(ReadOnlyMemory<byte> deflatedInput)
+                => BclCompressionHelper.FlateToBuffer(deflatedInput.Span, CompressionMode.Decompress);
+
+            static ReadOnlyMemory<byte> ZlibInflate(ReadOnlyMemory<byte> deflatedInput)
+                => BclCompressionHelper.ZlibToBuffer(deflatedInput.Span, CompressionMode.Decompress);
+
+            static ReadOnlyMemory<byte> GzipInflate(ReadOnlyMemory<byte> deflatedInput)
+                => BclCompressionHelper.GzipToBuffer(deflatedInput.Span, CompressionMode.Decompress);
+        }
+
+        private static readonly Random _rand = new();
+        private static byte[] GetRandomBuffer(int length)
+        {
+            var input = new byte[length];
+            _rand.NextBytes(input);
+            return input;
+        }
+
+        [Theory]
+        [MemberData(nameof(Compressors), 0)]
+        [MemberData(nameof(Compressors), 6)]
+        [MemberData(nameof(Compressors), 9)]
+        public void CompressAndValidateWithBclTest(Compressor compressor, ReadOnlyMemory<byte> input, BclInflater bclInflater)
+        {
+            using (compressor)
+            {
+                using var outputOwner = compressor.Compress(input.Span, useUpperBound: true);
+                Assert.NotNull(outputOwner);
+
+                var bclInflated = bclInflater(outputOwner.Memory);
+                Assert.True(input.Span.SequenceEqual(bclInflated.Span));
+            }
+        }
+    }
+}
