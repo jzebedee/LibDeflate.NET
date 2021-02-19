c748bee13ad5738959670952d26268648011929b
J. Zebedee <zebedee@code.gripe>
2021-02-19 13:09:31 -0600

    Update DllName

--- src/LibDeflate/Imports/Constants.cs
+++ src/LibDeflate/Imports/Constants.cs
@@ -6,6 +6,6 @@ namespace LibDeflate.Imports
 {
     internal static class Constants
     {
-        public const string DllName = @"libdeflate-1.7-windows-x86_64-bin\libdeflate.dll";
+        public const string DllName = "libdeflate";
     }
 }
