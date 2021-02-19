5ae6372af8fa56986cad4067befbf2b5cdc4dd99
J. Zebedee <zebedee@code.gripe>
2021-02-18 23:13:52 -0600

    Update CustomMemoryAllocator tests

--- test/LibDeflate.Tests/CustomMemoryAllocatorTests.cs
+++ test/LibDeflate.Tests/CustomMemoryAllocatorTests.cs
@@ -8,35 +8,33 @@ namespace LibDeflate.Tests
 {
     public class CustomMemoryAllocatorTests
     {
+        private static int mallocCount = 0;
+        private static int freeCount = 0;
+
+        private static nint malloc(nuint len)
+        {
+            mallocCount++;
+            return Marshal.AllocHGlobal((nint)len);
+        }
+
+        private static void free(nint alloc)
+        {
+            freeCount++;
+            Marshal.FreeHGlobal(alloc);
+        }
+
         [Fact]
         public void UseCustomAllocatorsTest()
         {
             CustomMemoryAllocator.libdeflate_set_memory_allocator(malloc, free);
 
-            bool receivedmalloc = false, receivedfree = false;
-
             //allocate something
             var compressor = Compression.libdeflate_alloc_compressor(0);
-            Assert.True(receivedmalloc);
+            Assert.Equal(1, mallocCount);
 
             //free something
             Compression.libdeflate_free_compressor(compressor);
-            Assert.True(receivedfree);
-
-            return;
-
-            IntPtr malloc(UIntPtr len)
-            {
-                Debug.WriteLine("Received malloc_func request for {0} bytes", (int)len);
-                receivedmalloc = true;
-                return Marshal.AllocHGlobal((int)len);
-            }
-            void free(IntPtr alloc)
-            {
-                Debug.WriteLine("Received free_func request for alloc {0}", alloc);
-                receivedfree = true;
-                Marshal.FreeHGlobal(alloc);
-            }
+            Assert.Equal(1, freeCount);
         }
     }
 }
