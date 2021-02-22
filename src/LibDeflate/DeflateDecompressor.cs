76d7a6679c2ea50f545b949b07bf000e6f48bf9d
J. Zebedee <zebedee@code.gripe>
2021-02-21 22:24:34 -0600

    Fully implement Zlib and Gzip decompressors

--- src/LibDeflate/DeflateDecompressor.cs
+++ src/LibDeflate/DeflateDecompressor.cs
@@ -16,18 +16,22 @@ namespace LibDeflate
 
         protected override OperationStatus DecompressCore(ReadOnlySpan<byte> input, Span<byte> output, nuint uncompressedSize)
             => StatusFromResult(libdeflate_deflate_decompress(decompressor, MemoryMarshal.GetReference(input),
-                (nuint)input.Length, ref MemoryMarshal.GetReference(output), uncompressedSize, out Unsafe.NullRef<UIntPtr>()));
+                (nuint)input.Length, ref MemoryMarshal.GetReference(output), uncompressedSize,
+                actual_out_nbytes_ret: out Unsafe.NullRef<UIntPtr>()));
 
         protected override OperationStatus DecompressCore(ReadOnlySpan<byte> input, Span<byte> output, out nuint bytesWritten)
             => StatusFromResult(libdeflate_deflate_decompress(decompressor, MemoryMarshal.GetReference(input),
-                (nuint)input.Length, ref MemoryMarshal.GetReference(output), (nuint)output.Length, out bytesWritten));
+                (nuint)input.Length, ref MemoryMarshal.GetReference(output), (nuint)output.Length,
+                actual_out_nbytes_ret: out bytesWritten));
 
         protected override OperationStatus DecompressCore(ReadOnlySpan<byte> input, Span<byte> output, nuint uncompressedSize, out nuint bytesRead)
             => StatusFromResult(libdeflate_deflate_decompress_ex(decompressor, MemoryMarshal.GetReference(input),
-                (nuint)input.Length, ref MemoryMarshal.GetReference(output), uncompressedSize, out bytesRead, out Unsafe.NullRef<UIntPtr>()));
+                (nuint)input.Length, ref MemoryMarshal.GetReference(output), uncompressedSize, out bytesRead,
+                actual_out_nbytes_ret: out Unsafe.NullRef<UIntPtr>()));
 
         protected override OperationStatus DecompressCore(ReadOnlySpan<byte> input, Span<byte> output, out nuint bytesWritten, out nuint bytesRead)
             => StatusFromResult(libdeflate_deflate_decompress_ex(decompressor, MemoryMarshal.GetReference(input),
-                (nuint)input.Length, ref MemoryMarshal.GetReference(output), (nuint)output.Length, out bytesRead, out bytesWritten));
+                (nuint)input.Length, ref MemoryMarshal.GetReference(output), (nuint)output.Length, out bytesRead,
+                actual_out_nbytes_ret: out bytesWritten));
     }
 }
