4d848dc6e3e4dd78a3547f3eda75dbb6a5068df7
J. Zebedee <zebedee@code.gripe>
2021-02-21 22:24:34 -0600

    Fully implement Zlib and Gzip decompressors

---
+++ src/LibDeflate/GzipDecompressor.cs
@@ -0,0 +1,33 @@
+﻿using LibDeflate.Imports;
+using System;
+using System.Buffers;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace LibDeflate
+{
+    using static Decompression;
+
+    public sealed class GzipDecompressor : Decompressor
+    {
+        public GzipDecompressor() : base()
+        {
+        }
+
+        protected override OperationStatus DecompressCore(ReadOnlySpan<byte> input, Span<byte> output, nuint uncompressedSize)
+            => StatusFromResult(libdeflate_gzip_decompress(decompressor, MemoryMarshal.GetReference(input),
+                (nuint)input.Length, ref MemoryMarshal.GetReference(output), uncompressedSize, out Unsafe.NullRef<UIntPtr>()));
+
+        protected override OperationStatus DecompressCore(ReadOnlySpan<byte> input, Span<byte> output, out nuint bytesWritten)
+            => StatusFromResult(libdeflate_gzip_decompress(decompressor, MemoryMarshal.GetReference(input),
+                (nuint)input.Length, ref MemoryMarshal.GetReference(output), (nuint)output.Length, out bytesWritten));
+
+        protected override OperationStatus DecompressCore(ReadOnlySpan<byte> input, Span<byte> output, nuint uncompressedSize, out nuint bytesRead)
+            => StatusFromResult(libdeflate_gzip_decompress_ex(decompressor, MemoryMarshal.GetReference(input),
+                (nuint)input.Length, ref MemoryMarshal.GetReference(output), uncompressedSize, out bytesRead, out Unsafe.NullRef<UIntPtr>()));
+
+        protected override OperationStatus DecompressCore(ReadOnlySpan<byte> input, Span<byte> output, out nuint bytesWritten, out nuint bytesRead)
+            => StatusFromResult(libdeflate_gzip_decompress_ex(decompressor, MemoryMarshal.GetReference(input),
+                (nuint)input.Length, ref MemoryMarshal.GetReference(output), (nuint)output.Length, out bytesRead, out bytesWritten));
+    }
+}
---
+++ src/LibDeflate/ZlibDecompressor.cs
@@ -0,0 +1,33 @@
+﻿using LibDeflate.Imports;
+using System;
+using System.Buffers;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace LibDeflate
+{
+    using static Decompression;
+
+    public sealed class ZlibDecompressor : Decompressor
+    {
+        public ZlibDecompressor() : base()
+        {
+        }
+
+        protected override OperationStatus DecompressCore(ReadOnlySpan<byte> input, Span<byte> output, nuint uncompressedSize)
+            => StatusFromResult(libdeflate_zlib_decompress(decompressor, MemoryMarshal.GetReference(input),
+                (nuint)input.Length, ref MemoryMarshal.GetReference(output), uncompressedSize, out Unsafe.NullRef<UIntPtr>()));
+
+        protected override OperationStatus DecompressCore(ReadOnlySpan<byte> input, Span<byte> output, out nuint bytesWritten)
+            => StatusFromResult(libdeflate_zlib_decompress(decompressor, MemoryMarshal.GetReference(input),
+                (nuint)input.Length, ref MemoryMarshal.GetReference(output), (nuint)output.Length, out bytesWritten));
+
+        protected override OperationStatus DecompressCore(ReadOnlySpan<byte> input, Span<byte> output, nuint uncompressedSize, out nuint bytesRead)
+            => StatusFromResult(libdeflate_zlib_decompress_ex(decompressor, MemoryMarshal.GetReference(input),
+                (nuint)input.Length, ref MemoryMarshal.GetReference(output), uncompressedSize, out bytesRead, out Unsafe.NullRef<UIntPtr>()));
+
+        protected override OperationStatus DecompressCore(ReadOnlySpan<byte> input, Span<byte> output, out nuint bytesWritten, out nuint bytesRead)
+            => StatusFromResult(libdeflate_zlib_decompress_ex(decompressor, MemoryMarshal.GetReference(input),
+                (nuint)input.Length, ref MemoryMarshal.GetReference(output), (nuint)output.Length, out bytesRead, out bytesWritten));
+    }
+}
