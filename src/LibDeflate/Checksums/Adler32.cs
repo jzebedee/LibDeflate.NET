391e4f8bfdf33f08171157b36bcc5a1f2d268f83
J. Zebedee <zebedee@code.gripe>
2021-02-22 06:39:23 -0600

    Add Adler32

---
+++ src/LibDeflate/Checksums/Adler32.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace LibDeflate.Checksums
+{
+    using static Imports.Checksums;
+
+    public struct Adler32
+    {
+        private bool _initialized;
+        //because we have to supply 1 as the initial value
+        //we must always retrieve this through Hash
+        private uint _currentAdler;
+        public uint Hash
+        {
+            get
+            {
+                if(!_initialized)
+                {
+                    _currentAdler = 1;
+                    _initialized = true;
+                }
+                return _currentAdler;
+            }
+        }
+
+        public void Append(ReadOnlySpan<byte> input)
+            => _currentAdler = AppendCore(Hash, input);
+
+        public uint Compute(ReadOnlySpan<byte> input)
+            => _currentAdler = AppendCore(1, input);
+
+        private static uint AppendCore(uint adler, ReadOnlySpan<byte> input)
+            => libdeflate_adler32(adler, MemoryMarshal.GetReference(input), (nuint)input.Length);
+    }
+}
