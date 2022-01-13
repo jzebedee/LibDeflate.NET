924c5536cade40da8c2a2d5770b3ae659e474777
J. Zebedee <zebedee@code.gripe>
2022-01-13 17:10:36 -0500

    Use file-scoped namespace in CustomMemoryAllocator

--- src/LibDeflate/Imports/CustomMemoryAllocator.cs
+++ src/LibDeflate/Imports/CustomMemoryAllocator.cs
@@ -1,26 +1,25 @@
 ﻿using System;
 using System.Runtime.InteropServices;
 
-namespace LibDeflate.Imports
+namespace LibDeflate.Imports;
+
+using size_t = UIntPtr;
+
+internal static class CustomMemoryAllocator
 {
-    using size_t = System.UIntPtr;
+    //[UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+    public delegate IntPtr malloc_func(size_t size);
+    //[UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+    public delegate void free_func(IntPtr alloc);
 
-    internal static class CustomMemoryAllocator
-    {
-        //[UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-        public delegate IntPtr malloc_func(size_t size);
-        //[UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-        public delegate void free_func(IntPtr alloc);
-
-        ///<summary>
-        /// Install a custom memory allocator which libdeflate will use for all memory
-        /// allocations.  'malloc_func' is a function that must behave like malloc(), and
-        /// 'free_func' is a function that must behave like free().
-        ///
-        /// There must not be any libdeflate_compressor or libdeflate_decompressor
-        /// structures in existence when calling this function.
-        ///</summary>
-        [DllImport(Constants.DllName, CallingConvention = CallingConvention.StdCall)]
-        public static extern void libdeflate_set_memory_allocator(malloc_func malloc, free_func free);
-    }
+    ///<summary>
+    /// Install a custom memory allocator which libdeflate will use for all memory
+    /// allocations.  'malloc_func' is a function that must behave like malloc(), and
+    /// 'free_func' is a function that must behave like free().
+    ///
+    /// There must not be any libdeflate_compressor or libdeflate_decompressor
+    /// structures in existence when calling this function.
+    ///</summary>
+    [DllImport(Constants.DllName, CallingConvention = CallingConvention.StdCall)]
+    public static extern void libdeflate_set_memory_allocator(malloc_func malloc, free_func free);
 }
