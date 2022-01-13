8665ae40b52fc68934954709f6bc11d0bf0a07f1
J. Zebedee <zebedee@code.gripe>
2022-01-13 17:08:57 -0500

    Use file-scoped namespace in Adler32

--- src/LibDeflate/Checksums/Adler32.cs
+++ src/LibDeflate/Checksums/Adler32.cs
@@ -1,36 +1,35 @@
 ﻿using System;
 using System.Runtime.InteropServices;
 
-namespace LibDeflate.Checksums
+namespace LibDeflate.Checksums;
+
+using static Imports.Checksums;
+
+public struct Adler32
 {
-    using static Imports.Checksums;
-
-    public struct Adler32
+    private bool _initialized;
+    //because we have to supply 1 as the initial value
+    //we must always retrieve this through Hash
+    private uint _currentAdler;
+    public uint Hash
     {
-        private bool _initialized;
-        //because we have to supply 1 as the initial value
-        //we must always retrieve this through Hash
-        private uint _currentAdler;
-        public uint Hash
+        get
         {
-            get
+            if (!_initialized)
             {
-                if(!_initialized)
-                {
-                    _currentAdler = 1;
-                    _initialized = true;
-                }
-                return _currentAdler;
+                _currentAdler = 1;
+                _initialized = true;
             }
+            return _currentAdler;
         }
-
-        public void Append(ReadOnlySpan<byte> input)
-            => _currentAdler = AppendCore(Hash, input);
-
-        public uint Compute(ReadOnlySpan<byte> input)
-            => _currentAdler = AppendCore(1, input);
-
-        private static uint AppendCore(uint adler, ReadOnlySpan<byte> input)
-            => libdeflate_adler32(adler, MemoryMarshal.GetReference(input), (nuint)input.Length);
     }
+
+    public void Append(ReadOnlySpan<byte> input)
+        => _currentAdler = AppendCore(Hash, input);
+
+    public uint Compute(ReadOnlySpan<byte> input)
+        => _currentAdler = AppendCore(1, input);
+
+    private static uint AppendCore(uint adler, ReadOnlySpan<byte> input)
+        => libdeflate_adler32(adler, MemoryMarshal.GetReference(input), (nuint)input.Length);
 }
