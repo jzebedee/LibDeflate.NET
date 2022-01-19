eca79f2a49bcd32bdea3669c5206e2d76dd4e1a5
J. Zebedee <zebedee@code.gripe>
2022-01-18 22:47:18 -0500

    Github actions (#6)
    * Add gh-actions stubs
    
    * Update CI
    
    * Update solution
    
    * Add benchmark stub (fixes build)
    
    * Update CI
    
    * Update release
    
    * Fix using the wrong context for run_id
    
    https://docs.github.com/en/actions/learn-github-actions/contexts#github-context

---
+++ bench/LibDeflate.Benchmarks/Program.cs
@@ -0,0 +1,8 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace LibDeflate.Benchmarks;
+
+public class Program
+{
+    public static void Main(string[] args) => BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+}
