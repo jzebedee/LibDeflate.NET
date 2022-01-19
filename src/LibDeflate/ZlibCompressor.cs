3481eecbc288241044f962c9fcf3c5277050549f
J. Zebedee <zebedee@code.gripe>
2022-01-19 00:51:54 -0500

    Use file-scoped namespaces

--- src/LibDeflate/ZlibCompressor.cs
+++ src/LibDeflate/ZlibCompressor.cs
@@ -2,18 +2,17 @@
 using System;
 using System.Runtime.InteropServices;
 
-namespace LibDeflate
+namespace LibDeflate;
+
+public sealed class ZlibCompressor : Compressor
 {
-    public sealed class ZlibCompressor : Compressor
+    public ZlibCompressor(int compressionLevel) : base(compressionLevel)
     {
-        public ZlibCompressor(int compressionLevel) : base(compressionLevel)
-        {
-        }
-
-        protected override nuint CompressCore(ReadOnlySpan<byte> input, Span<byte> output)
-            => Compression.libdeflate_zlib_compress(compressor, MemoryMarshal.GetReference(input), (nuint)input.Length, ref MemoryMarshal.GetReference(output), (nuint)output.Length);
-
-        protected override nuint GetBoundCore(nuint inputLength)
-            => Compression.libdeflate_zlib_compress_bound(compressor, inputLength);
     }
+
+    protected override nuint CompressCore(ReadOnlySpan<byte> input, Span<byte> output)
+        => Compression.libdeflate_zlib_compress(compressor, MemoryMarshal.GetReference(input), (nuint)input.Length, ref MemoryMarshal.GetReference(output), (nuint)output.Length);
+
+    protected override nuint GetBoundCore(nuint inputLength)
+        => Compression.libdeflate_zlib_compress_bound(compressor, inputLength);
 }
