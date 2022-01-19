68fe38d8150f346621660f7492fee04f6103c358
J. Zebedee <zebedee@code.gripe>
2022-01-18 22:14:13 -0500

    Use file-scoped namespaces

--- src/LibDeflate/Imports/Checksums.cs
+++ src/LibDeflate/Imports/Checksums.cs
@@ -1,28 +1,27 @@
 ﻿using System;
 using System.Runtime.InteropServices;
 
-namespace LibDeflate.Imports
+namespace LibDeflate.Imports;
+
+using size_t = System.UIntPtr;
+
+internal static class Checksums
 {
-    using size_t = System.UIntPtr;
+    ///<summary>
+    /// libdeflate_adler32() updates a running Adler-32 checksum with 'len' bytes of
+    /// data and returns the updated checksum.  When starting a new checksum, the
+    /// required initial value for 'adler' is 1.  This value is also returned when
+    /// 'buffer' is specified as NULL.
+    ///</summary>
+    [DllImport(Constants.DllName, CallingConvention = CallingConvention.StdCall)]
+    public static extern UInt32 libdeflate_adler32(UInt32 adler, in byte buffer, size_t len);
 
-    internal static class Checksums
-    {
-        ///<summary>
-        /// libdeflate_adler32() updates a running Adler-32 checksum with 'len' bytes of
-        /// data and returns the updated checksum.  When starting a new checksum, the
-        /// required initial value for 'adler' is 1.  This value is also returned when
-        /// 'buffer' is specified as NULL.
-        ///</summary>
-        [DllImport(Constants.DllName, CallingConvention = CallingConvention.StdCall)]
-        public static extern UInt32 libdeflate_adler32(UInt32 adler, in byte buffer, size_t len);
-
-        ///<summary>
-        /// libdeflate_crc32() updates a running CRC-32 checksum with 'len' bytes of data
-        /// and returns the updated checksum.  When starting a new checksum, the required
-        /// initial value for 'crc' is 0.  This value is also returned when 'buffer' is
-        /// specified as NULL.
-        ///</summary>
-        [DllImport(Constants.DllName, CallingConvention = CallingConvention.StdCall)]
-        public static extern UInt32 libdeflate_crc32(UInt32 crc, in byte buffer, size_t len);
-    }
+    ///<summary>
+    /// libdeflate_crc32() updates a running CRC-32 checksum with 'len' bytes of data
+    /// and returns the updated checksum.  When starting a new checksum, the required
+    /// initial value for 'crc' is 0.  This value is also returned when 'buffer' is
+    /// specified as NULL.
+    ///</summary>
+    [DllImport(Constants.DllName, CallingConvention = CallingConvention.StdCall)]
+    public static extern UInt32 libdeflate_crc32(UInt32 crc, in byte buffer, size_t len);
 }
--- test/LibDeflate.Tests/BclCompressionHelper.cs
+++ test/LibDeflate.Tests/BclCompressionHelper.cs
@@ -2,94 +2,92 @@
 using System;
 using System.IO;
 using System.IO.Compression;
-using System.Runtime.InteropServices;
 
-namespace LibDeflate.Tests
+namespace LibDeflate.Tests;
+
+internal static class BclCompressionHelper
 {
-    internal static class BclCompressionHelper
+    internal static MemoryStream CopySpanToMemoryStream(ReadOnlySpan<byte> input)
     {
-        internal static MemoryStream CopySpanToMemoryStream(ReadOnlySpan<byte> input)
+        var ms = new MemoryStream();
+        ms.Write(input);
+        ms.Seek(0, SeekOrigin.Begin);
+        return ms;
+    }
+
+    internal static ReadOnlyMemory<byte> FlateToBuffer(ReadOnlySpan<byte> input, CompressionMode mode)
+    {
+        var outputMs = new MemoryStream();
+
+        switch (mode)
         {
-            var ms = new MemoryStream();
-            ms.Write(input);
-            ms.Seek(0, SeekOrigin.Begin);
-            return ms;
+            case CompressionMode.Compress:
+                using (var flateStream = new DeflateStream(outputMs, mode, true))
+                {
+                    flateStream.Write(input);
+                    flateStream.Flush();
+                }
+                break;
+            case CompressionMode.Decompress:
+                using (var inputMs = CopySpanToMemoryStream(input))
+                using (var flateStream = new DeflateStream(inputMs, mode))
+                {
+                    flateStream.CopyTo(outputMs);
+                }
+                break;
         }
 
-        internal static ReadOnlyMemory<byte> FlateToBuffer(ReadOnlySpan<byte> input, CompressionMode mode)
+        return new ReadOnlyMemory<byte>(outputMs.GetBuffer(), 0, (int)outputMs.Length);
+    }
+
+    internal static ReadOnlyMemory<byte> ZlibToBuffer(ReadOnlySpan<byte> input, CompressionMode mode)
+    {
+        var outputMs = new MemoryStream();
+
+        switch (mode)
         {
-            var outputMs = new MemoryStream();
-
-            switch (mode)
-            {
-                case CompressionMode.Compress:
-                    using (var flateStream = new DeflateStream(outputMs, mode, true))
-                    {
-                        flateStream.Write(input);
-                        flateStream.Flush();
-                    }
-                    break;
-                case CompressionMode.Decompress:
-                    using (var inputMs = CopySpanToMemoryStream(input))
-                    using (var flateStream = new DeflateStream(inputMs, mode))
-                    {
-                        flateStream.CopyTo(outputMs);
-                    }
-                    break;
-            }
-
-            return new ReadOnlyMemory<byte>(outputMs.GetBuffer(), 0, (int)outputMs.Length);
+            case CompressionMode.Compress:
+                using (var zlibStream = new ZlibOutputStream(outputMs, SixLabors.ZlibStream.CompressionLevel.DefaultCompression))
+                {
+                    zlibStream.Write(input);
+                    zlibStream.Flush();
+                }
+                break;
+            case CompressionMode.Decompress:
+                using (var inputMs = CopySpanToMemoryStream(input))
+                using (var zlibStream = new ZlibInputStream(inputMs))
+                {
+                    zlibStream.CopyTo(outputMs);
+                }
+                break;
         }
 
-        internal static ReadOnlyMemory<byte> ZlibToBuffer(ReadOnlySpan<byte> input, CompressionMode mode)
+        return new ReadOnlyMemory<byte>(outputMs.GetBuffer(), 0, (int)outputMs.Length);
+    }
+
+
+    internal static ReadOnlyMemory<byte> GzipToBuffer(ReadOnlySpan<byte> input, CompressionMode mode)
+    {
+        var outputMs = new MemoryStream();
+
+        switch (mode)
         {
-            var outputMs = new MemoryStream();
-
-            switch (mode)
-            {
-                case CompressionMode.Compress:
-                    using (var zlibStream = new ZlibOutputStream(outputMs, SixLabors.ZlibStream.CompressionLevel.DefaultCompression))
-                    {
-                        zlibStream.Write(input);
-                        zlibStream.Flush();
-                    }
-                    break;
-                case CompressionMode.Decompress:
-                    using (var inputMs = CopySpanToMemoryStream(input))
-                    using (var zlibStream = new ZlibInputStream(inputMs))
-                    {
-                        zlibStream.CopyTo(outputMs);
-                    }
-                    break;
-            }
-
-            return new ReadOnlyMemory<byte>(outputMs.GetBuffer(), 0, (int)outputMs.Length);
+            case CompressionMode.Compress:
+                using (var flateStream = new GZipStream(outputMs, mode, true))
+                {
+                    flateStream.Write(input);
+                    flateStream.Flush();
+                }
+                break;
+            case CompressionMode.Decompress:
+                using (var inputMs = CopySpanToMemoryStream(input))
+                using (var flateStream = new GZipStream(inputMs, mode))
+                {
+                    flateStream.CopyTo(outputMs);
+                }
+                break;
         }
 
-
-        internal static ReadOnlyMemory<byte> GzipToBuffer(ReadOnlySpan<byte> input, CompressionMode mode)
-        {
-            var outputMs = new MemoryStream();
-
-            switch (mode)
-            {
-                case CompressionMode.Compress:
-                    using (var flateStream = new GZipStream(outputMs, mode, true))
-                    {
-                        flateStream.Write(input);
-                        flateStream.Flush();
-                    }
-                    break;
-                case CompressionMode.Decompress:
-                    using (var inputMs = CopySpanToMemoryStream(input))
-                    using (var flateStream = new GZipStream(inputMs, mode))
-                    {
-                        flateStream.CopyTo(outputMs);
-                    }
-                    break;
-            }
-
-            return new ReadOnlyMemory<byte>(outputMs.GetBuffer(), 0, (int)outputMs.Length);
-        }
+        return new ReadOnlyMemory<byte>(outputMs.GetBuffer(), 0, (int)outputMs.Length);
     }
 }
