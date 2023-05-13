6570dcea4f77590b948a332eb186b30fb2bb2d24
J. Zebedee <zebedee@code.gripe>
2023-05-13 14:41:23 -0500

    Use file-scoped namespaces

--- src/LibDeflate/GzipCompressor.cs
+++ src/LibDeflate/GzipCompressor.cs
@@ -2,18 +2,17 @@
 using System;
 using System.Runtime.InteropServices;
 
-namespace LibDeflate
+namespace LibDeflate;
+
+public sealed class GzipCompressor : Compressor
 {
-    public sealed class GzipCompressor : Compressor
+    public GzipCompressor(int compressionLevel) : base(compressionLevel)
     {
-        public GzipCompressor(int compressionLevel) : base(compressionLevel)
-        {
-        }
-
-        protected override nuint CompressCore(ReadOnlySpan<byte> input, Span<byte> output)
-            => Compression.libdeflate_gzip_compress(compressor, MemoryMarshal.GetReference(input), (nuint)input.Length, ref MemoryMarshal.GetReference(output), (nuint)output.Length);
-
-        protected override nuint GetBoundCore(nuint inputLength)
-            => Compression.libdeflate_gzip_compress_bound(compressor, inputLength);
     }
+
+    protected override nuint CompressCore(ReadOnlySpan<byte> input, Span<byte> output)
+        => Compression.libdeflate_gzip_compress(compressor, MemoryMarshal.GetReference(input), (nuint)input.Length, ref MemoryMarshal.GetReference(output), (nuint)output.Length);
+
+    protected override nuint GetBoundCore(nuint inputLength)
+        => Compression.libdeflate_gzip_compress_bound(compressor, inputLength);
 }
