2310e9d31953f040bd2d23b30d6dfa70a476276f
J. Zebedee <zebedee@code.gripe>
2023-05-13 14:43:18 -0500

    Update bench to use P&C texts

--- bench/LibDeflate.Benchmarks/DeflateCompressorBenchmarks.cs
+++ bench/LibDeflate.Benchmarks/DeflateCompressorBenchmarks.cs
@@ -7,48 +7,32 @@ using System.Linq;
 
 namespace LibDeflate.Benchmarks;
 
+[MemoryDiagnoser]
+[SimpleJob]
 public class DeflateCompressorBenchmarks
 {
-    private static Random GetRepeatableRandom() => new(63 * 13 * 17 * 13);
-    private static byte[] GetRandomBuffer(int length)
-    {
-        var rand = GetRepeatableRandom();
-        var input = new byte[length];
-        rand.NextBytes(input);
-        return input;
-    }
-
-    public static IEnumerable<object[]> Inputs => from buffer in Buffers
+    public static IEnumerable<object[]> Inputs => from key in TestFiles.Keys
                                                   from level in Levels
-                                                  select new object[] { buffer, level };
+                                                  select new object[] { key, level };
 
-    private static IEnumerable<byte[]> Buffers
-    {
-        get
-        {
-            yield return GetRandomBuffer(0);
-            yield return GetRandomBuffer(1 << 0);
-            yield return GetRandomBuffer(1 << 4);
-            yield return GetRandomBuffer(1 << 8);
-            yield return GetRandomBuffer(1 << 16);
-            //yield return GetRandomBuffer(int.MaxValue);
-        }
-    }
+    private static Dictionary<string, byte[]> TestFiles { get; } = Directory.EnumerateFiles(@"texts/")
+                .Where(fn => !Path.GetExtension(fn).Equals(".gz", StringComparison.OrdinalIgnoreCase))
+                .ToDictionary(fn => Path.GetFileNameWithoutExtension(fn), File.ReadAllBytes);
 
     private static IEnumerable<int> Levels
     {
         get
         {
-            yield return 0;
+            //yield return 0;
             yield return 1;
-            yield return 6;
-            yield return 9;
+            //yield return 6;
+            //yield return 9;
         }
     }
 
     [Benchmark(Baseline = true)]
     [ArgumentsSource(nameof(Inputs))]
-    public void DeflateSIO(byte[] input, int level)
+    public void DeflateSIO(string testFile, int level)
     {
         var compressionLevel = level switch
         {
@@ -58,6 +42,7 @@ public class DeflateCompressorBenchmarks
             9 => CompressionLevel.SmallestSize
         };
 
+        var input = TestFiles[testFile];
         using var outputMs = new MemoryStream(input.Length);
         using var deflateStream = new DeflateStream(outputMs, compressionLevel);
         deflateStream.Write(input);
@@ -65,8 +50,9 @@ public class DeflateCompressorBenchmarks
 
     [Benchmark]
     [ArgumentsSource(nameof(Inputs))]
-    public void DeflateLibdeflate(byte[] input, int level)
+    public void DeflateLibdeflate(string testFile, int level)
     {
+        var input = TestFiles[testFile];
         using var compressor = new DeflateCompressor(level);
         using var owner = compressor.Compress(input);
     }
