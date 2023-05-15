d04096290e975ed3c503fbabcfa2233fa6eb0972
J. Zebedee <zebedee@code.gripe>
2023-05-14 19:33:48 -0500

    Add function pointer overload of libdeflate_set_memory_allocator and tests

--- src/LibDeflate/Imports/CustomMemoryAllocator.cs
+++ src/LibDeflate/Imports/CustomMemoryAllocator.cs
@@ -25,4 +25,15 @@ internal static partial class CustomMemoryAllocator
     [UnmanagedCallConv(CallConvs = new[] { typeof(CallConvCdecl) })]
     public static partial void libdeflate_set_memory_allocator(malloc_func malloc, free_func free);
 
+    ///<summary>
+    /// Install a custom memory allocator which libdeflate will use for all memory
+    /// allocations.  'malloc_func' is a function that must behave like malloc(), and
+    /// 'free_func' is a function that must behave like free().
+    ///
+    /// There must not be any libdeflate_compressor or libdeflate_decompressor
+    /// structures in existence when calling this function.
+    ///</summary>
+    [LibraryImport(Constants.DllName, EntryPoint = "libdeflate_set_memory_allocator")]
+    [UnmanagedCallConv(CallConvs = new[] { typeof(CallConvCdecl) })]
+    public static unsafe partial void libdeflate_set_memory_allocator_unsafe(delegate* unmanaged[Cdecl]<size_t, void*> malloc, delegate* unmanaged[Cdecl]<void*, void> free);
 }
