d4027101b991b43c09e208244927485a7274e049
J. Zebedee <zebedee@code.gripe>
2023-05-14 20:36:26 -0500

    Use top-level main in benchmarks project

--- bench/LibDeflate.Benchmarks/Program.cs
+++ bench/LibDeflate.Benchmarks/Program.cs
@@ -1,8 +1,3 @@
 ﻿using BenchmarkDotNet.Running;
 
-namespace LibDeflate.Benchmarks;
-
-public class Program
-{
-    public static void Main(string[] args) => BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
-}
+BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
