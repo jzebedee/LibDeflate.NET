9a8da23ed17701e0a33074ffa0d7c0cc74f4db76
J. Zebedee <zebedee@code.gripe>
2023-05-14 20:35:44 -0500

    Make benchmarks a friend of LibDeflate

--- src/LibDeflate/Imports/Constants.cs
+++ src/LibDeflate/Imports/Constants.cs
@@ -3,6 +3,7 @@ using System.Runtime.InteropServices;
 
 [assembly: InternalsVisibleTo($"{nameof(LibDeflate)}.Tests")]
 [assembly: InternalsVisibleTo($"{nameof(LibDeflate)}.DangerousTests")]
+[assembly: InternalsVisibleTo($"{nameof(LibDeflate)}.Benchmarks")]
 namespace LibDeflate.Imports;
 
 internal static class Constants
