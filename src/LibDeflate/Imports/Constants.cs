16558da7905e2950cf0bb2c1179a5e8836254794
J. Zebedee <zebedee@code.gripe>
2024-01-28 12:19:59 -0600

    Disable runtime marshalling

--- src/LibDeflate/Imports/Constants.cs
+++ src/LibDeflate/Imports/Constants.cs
@@ -3,6 +3,8 @@
 [assembly: InternalsVisibleTo($"{nameof(LibDeflate)}.Tests")]
 [assembly: InternalsVisibleTo($"{nameof(LibDeflate)}.DangerousTests")]
 [assembly: InternalsVisibleTo($"{nameof(LibDeflate)}.Benchmarks")]
+[assembly: DisableRuntimeMarshalling]
+
 namespace LibDeflate.Imports;
 
 internal static class Constants
